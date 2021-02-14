061f2dea880d59677296f40ffaeb1e58507a15a2
jasbirrajrana <jasbirrajrana6699@gmail.com>
2021-02-14 16:40:06 +0530

    ready to deploy on heroku

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -10,7 +10,6 @@ const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email });
-  console.log(email, password);
   if (user && (await user.matchPassword(password))) {
     return res.json({
       _id: user._id,
